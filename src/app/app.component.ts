ac89aed683f90437faf3879f8bc359e6945cf404
Kavshree <kavyashri.bj@gmail.com>
2020-10-30 20:52:06 -0400

    added new files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myPortfolio';
+  myFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+
+    if (scrolled > 10) {
+      document.getElementById("mainID").style.fontSize = "30px";
+    } else {
+      document.getElementById("mainID").style.fontSize = "50px";
+    }
+  }
+
+  // When the user scrolls the page, execute myFunction 
+
+
+@HostListener('window:scroll', ['$event']) // for window scroll events
+onScroll(event) {
+  this.myFunction()
+
+}
+
 }
